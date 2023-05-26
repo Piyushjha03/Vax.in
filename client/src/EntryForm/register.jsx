179fb7a35fa94117fde05b5af0b3453ceb58f4a6
Piyushjha03 <95981927+Piyushjha03@users.noreply.github.com>
2023-05-26 11:09:09 +0530

    first_commit

---
+++ client/src/EntryForm/register.jsx
@@ -0,0 +1,72 @@
+import { Link } from "react-router-dom"
+import React, { useState } from "react"
+import { Login } from "./login";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { DatePicker } from '@y0c/react-datepicker';
+ import '@y0c/react-datepicker/assets/styles/calendar.scss';
+export const Register=(props)=>{
+
+
+    const[userDetails,setUserDetails]=useState({
+        firstName:"",
+        lastName:"",
+        phnumber:"",
+        dob:"",
+        address:"",
+        aadhar_number:"",
+        gender:"",
+        password:"",
+    });
+
+    const handleChange=(e)=>{
+        setUserDetails((prev)=>({...prev,[e.target.name]:e.target.value}));
+    }
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+    }
+
+const handleClick=async(e)=>{
+    e.preventDefault();
+    try {
+        await axios.post("http://localhost:8800/register",userDetails);
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+const onChangeDp=(date)=>{
+const todate=`${date.$y}-${(date.$M)+1}-${date.$D}`;  
+console.log(todate);
+setUserDetails((prev)=>({...prev,dob:todate}));
+
+}
+
+    return(
+        <>
+        <div className="register-page">
+        <div className="register-text">REGISTER</div>
+        
+
+            <form className="register-page-form" onSubmit={handleSubmit}>
+                <input type="text"  placeholder="First Name" className="firstName" name="firstName" onChange={handleChange}/>
+                <input type="text"  placeholder="Last Name" className="lastName" name="lastName" onChange={handleChange}/>
+                <input type="tel"  placeholder="Phone Number" className="phnumber" name="phnumber" onChange={handleChange}/>
+                {/* <input type="date"  placeholder="Date of Birth" className="dob" name="dob" onChange={handleChange}/> */}
+                <DatePicker className="dob" placeholder="Date of birth" onChange={onChangeDp}  />
+                <input type="text"  placeholder="Address" className="address" name="address" onChange={handleChange}/>
+                <input type="text"  placeholder="Aadhar Number" className="aadhar_number" name="aadhar_number" onChange={handleChange}/>
+                <input type="text"  placeholder="M/F" className="gender" name="gender" onChange={handleChange}/>
+                <input type="password"  placeholder="password" className="password" name="password" onChange={handleChange}/>
+                <button onClick={handleClick} className="create-bttn" type="submit">Create Account</button>
+            </form>
+ 
+            
+            <button onClick={()=>{ props.toggle(0)}} className="login-bttn">LOGIN</button>
+        </div>       
+        </>
+    
+    )
+}
+   
