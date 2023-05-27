2c5cc957c0ff216c9a96e18782b36b5908412955
Piyushjha03 <95981927+Piyushjha03@users.noreply.github.com>
2023-05-27 15:00:53 +0530

    cors

--- api/index.js
+++ api/index.js
@@ -2,10 +2,12 @@
 import express from "express"
 import mysql from "mysql"
 import cors from "cors"
+import path from "path";
 
 const app=express();
+
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:"https://vaxin.vercel.app/"}));
 
 const db=mysql.createConnection({
     host:"localhost",
