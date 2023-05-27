97291f8ee054bae60f3cd59ffbeb3bce15f86cfe
Piyushjha03 <95981927+Piyushjha03@users.noreply.github.com>
2023-05-27 15:08:03 +0530

    allow origin *

--- api/index.js
+++ api/index.js
@@ -8,6 +8,12 @@ const app=express();
 
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  })
 
 const db=mysql.createConnection({
     host:"localhost",
