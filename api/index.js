8ad7b0054e8e92ed953378c6593c6c967cc7db9f
Piyushjha03 <95981927+Piyushjha03@users.noreply.github.com>
2023-05-27 15:09:20 +0530

    origin

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ import path from "path";
 const app=express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:"https://vaxin.vercel.app"}));
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
